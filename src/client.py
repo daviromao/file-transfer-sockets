5edd580b98cb860f42210d5ced4efad03926663b
Davi Romão <davi.romao0210@gmail.com>
2024-03-19 16:46:38 -0300

    client send file function

--- src/client.py
+++ src/client.py
@@ -1,12 +1,13 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from constants import *
 
-HOST = ''
+HOST = '172.20.8.236'
 PORT = 9999
 
 def get_file_list():
     client = socket(AF_INET, SOCK_STREAM)
     client.connect((HOST, PORT))
+    
     request_type = FILE_LIST.to_bytes(1, byteorder='big')
     client.send(request_type)
     
@@ -16,3 +17,25 @@ def get_file_list():
         data += buffer
     files = data.decode("utf-8")
     return files
+
+
+def send_file(filepath):
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect((HOST, PORT))
+    
+    request_type = SEND_FILE.to_bytes(1, byteorder='big')
+    client.send(request_type)
+
+    filename = filepath.split("/")[-1]
+    filename_size = len(filename).to_bytes(length=4, byteorder='big')
+    
+    client.send(filename_size)
+    client.send(filename.encode("utf-8"))
+    
+    with open(filepath, "rb") as file:
+        buffer = file.read(1024)
+        while(buffer):
+            client.send(buffer)
+            buffer = file.read(1024)
+    
+    client.close()
